512b7562a2eaa49474e2d485ae6cf7aa8147c30b
Jacky Zhang <yzhang69@email.wm.edu>
2022-08-29 16:48:07 -0400

    added crawler method (first stock)

--- crawler.py
+++ crawler.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3.9
 import smtplib
+import time
 from email.mime.text import MIMEText
 from email.utils import formataddr
+from datetime import date
+from urllib import response
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
 # sender's email address
-sender=''  
+sender='992530621@qq.com'  
 # sender's email smtp cpde
 smtp_code = ''   
 # receiver's email address
-receiver=''   
-def mail():
+receiver='jackyzhang0912@gmail.com'
+
+
+def sendMail(stockDict):
   ret=True
   try:
+    emailBody = ''
+    for key in stockDict:
+      emailBody += (getStockData(key, stockDict[key], True))  # currency needs work!!! Hard-coded for now
     # content of the email
-    msg=MIMEText('Testing!','plain','utf-8') 
+    msg=MIMEText(emailBody,'plain','utf-8') 
     # sender's nickname and email address
     msg['From']=formataddr(["jackyQQ",sender]) 
     # receiver's nickname and email address
@@ -26,13 +37,37 @@ def mail():
     # close connection
     server.quit() 
     # if codes in try didn't run, ret will be set to false 
-  except Exception: 
+  except Exception as e: 
+    print(e)
     ret=False
 
   return ret
 
-ret=mail()
+def getStockData(stockName, stockUrl, isForeignCurrency):
+  browser.get(stockUrl)
+  # time.sleep(17)
+  stock_price = browser.find_element("xpath", '//div[@class="zxj"]').text
+  stock_change_amount = browser.find_element("xpath", '//div[@class="zd"]/span[1]').text
+  stock_change_percent = browser.find_element("xpath", '//div[@class="zd"]/span[2]').text
+  if isForeignCurrency:
+    stock_currency = browser.find_element("xpath", '//span[@class="quote_title_money"]').text[5:]
+    return ('The stock price of ' + stockName + ' is ' + stock_price + '; ' + 'change percent is ' 
+    + stock_change_percent + ' with an amount change of ' + stock_change_amount + '; ' 'The currency used is ' + stock_currency + '\n')
+  return ('The stock price of ' + stockName + ' is ' + stock_price + '; ' + 'change percent is ' 
+    + stock_change_percent + ' with an amount change of ' + stock_change_amount + '\n')
+
+  
+
+# dictionary contains stocks' names and corresponding urls
+stockDict = {"Tencent" : "https://quote.eastmoney.com/hk/00700.html"}
+service = Service('/Users/skywalker/Downloads/chromedriver')
+browser = webdriver.Chrome(service=service)
+
+
+ret=sendMail(stockDict)
 if ret:
   print("Send successfully")
 else:
   print("Send failed")
+
+browser.close()
